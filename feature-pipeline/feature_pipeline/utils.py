56a2bfd2bb1171d2958890f00612c61a491d60de
audunkn <audun.klarholm.nilsen@gmail.com>
2024-12-27 14:27:40 +0100

    created def get_logger

--- feature-pipeline/feature_pipeline/utils.py
+++ feature-pipeline/feature_pipeline/utils.py
@@ -3,3 +3,18 @@ import logging
 from pathlib import Path
 
 from feature_pipeline import settings
+
+def get_logger(name: str) -> logging.Logger:
+    """
+    Template for getting a logger.
+
+    Args:
+        name: Name of the logger.
+
+    Returns: Logger.
+    """
+
+    logging.basicConfig(level=logging.INFO) # sensitivity level - captures all informational messages, warnings, and errors, but not debug messages
+    logger = logging.getLogger(name) # creates a new logger instance with the given name
+
+    return logger
