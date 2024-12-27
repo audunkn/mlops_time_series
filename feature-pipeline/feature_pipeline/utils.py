cdaad85f670d683f4279bff0f93a297e0a1d77ae
audunkn <audun.klarholm.nilsen@gmail.com>
2024-12-27 14:47:17 +0100

    added def load_json to utils.py

--- feature-pipeline/feature_pipeline/utils.py
+++ feature-pipeline/feature_pipeline/utils.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from feature_pipeline import settings
 
+
 def get_logger(name: str) -> logging.Logger:
     """
     Template for getting a logger.
@@ -19,6 +20,7 @@ def get_logger(name: str) -> logging.Logger:
 
     return logger
 
+
     def save_json(data: dict, file_name: str, save_dir: str = settings.OUTPUT_DIR): # settings.OUTPUT_DIR is default value for save_dir
     """
     Save a dictionary as a JSON file.
@@ -33,4 +35,23 @@ def get_logger(name: str) -> logging.Logger:
 
     data_path = Path(save_dir) / file_name # creates the complete file path by combining the directory and filename
     with open(data_path, "w") as f: # context manager (with statement) ensures the file is properly closed even if an error occurs
-        json.dump(data, f) 
+        json.dump(data, f)
+
+
+    def load_json(file_name: str, save_dir: str = settings.OUTPUT_DIR) -> dict: 
+    """
+    Load a JSON file.
+
+    Args:
+        file_name: Name of the JSON file.
+        save_dir: Directory of the JSON file.
+
+    Returns: Dictionary with the data.
+    """
+
+    data_path = Path(save_dir) / file_name
+    if not data_path.exists():
+        raise FileNotFoundError(f"Cached JSON from {data_path} does not exist.")
+
+    with open(data_path, "r") as f:
+        return json.load(f) 
