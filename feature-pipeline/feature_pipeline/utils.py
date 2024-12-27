4faf51815fb809158930c6d4f82cfc71d5098efe
audunkn <audun.klarholm.nilsen@gmail.com>
2024-12-27 14:38:29 +0100

    added def save_json to utils.py

--- feature-pipeline/feature_pipeline/utils.py
+++ feature-pipeline/feature_pipeline/utils.py
@@ -18,3 +18,19 @@ def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name) # creates a new logger instance with the given name
 
     return logger
+
+    def save_json(data: dict, file_name: str, save_dir: str = settings.OUTPUT_DIR): # settings.OUTPUT_DIR is default value for save_dir
+    """
+    Save a dictionary as a JSON file.
+
+    Args:
+        data: data to save.
+        file_name: Name of the JSON file.
+        save_dir: Directory to save the JSON file.
+
+    Returns: None
+    """
+
+    data_path = Path(save_dir) / file_name # creates the complete file path by combining the directory and filename
+    with open(data_path, "w") as f: # context manager (with statement) ensures the file is properly closed even if an error occurs
+        json.dump(data, f) 
