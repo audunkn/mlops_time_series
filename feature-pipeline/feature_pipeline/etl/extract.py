7d7022c5d96c6de529bfda2f65c22123cd947ad1
audunkn <audun.klarholm.nilsen@gmail.com>
2024-12-31 11:41:12 +0100

    created and completed def from_file

--- feature-pipeline/feature_pipeline/etl/extract.py
+++ feature-pipeline/feature_pipeline/etl/extract.py
@@ -11,6 +11,54 @@ from feature_pipeline import utils, settings
 
 logger = utils.get_logger(__name__)
 
+def from_file(
+    export_end_reference_datetime: Optional[datetime.datetime] = None
+    days_delay: int = 15,
+    days_export: int = 30,
+    url: str = "https://drive.google.com/uc?export=download&id=1y48YeDymLurOTUO-GeFOUXVNc9MCApG5",
+    datetime_format: str = "%Y-%m-%d %H:%M",
+    cache_dir: Optional[Path] = None,
+    ) -> Optional[Tuple[pd.DataFrame, Dict[str, Any]]]:
+
+    """
+    Extract data from the DK energy consumption API.
+
+    As the official API expired in July 2023, we will use a copy of the data to simulate the same behavior. 
+    We made a copy of the data between '2020-06-30 22:00' and '2023-06-30 21:00'. Thus, there are 3 years of data to play with.
+
+    Here is the link to the official obsolete dataset: https://www.energidataservice.dk/tso-electricity/ConsumptionDE35Hour
+    Here is the link to the copy of the dataset: https://drive.google.com/file/d/1y48YeDymLurOTUO-GeFOUXVNc9MCApG5/view?usp=drive_link
+    
+    Args:
+        export_end_reference_datetime: The end reference datetime of the export window. If None, the current time is used.
+            Because the data is always delayed with "days_delay" days, this date is used only as a reference point.
+            The real extracted window will be computed as [export_end_reference_datetime - days_delay - days_export, export_end_reference_datetime - days_delay].
+        days_delay: Data has a delay of N days. Thus, we have to shift our window with N days.
+        days_export: The number of days to export.
+        url: The URL of the API.
+        datetime_format: The datetime format of the fields from the file.
+        cache_dir: The directory where the downloaded data will be cached. By default it will be downloaded in the standard output directory.
+
+
+    Returns:
+          A tuple of a Pandas DataFrame containing the exported data and a dictionary of metadata.
+    """
+
+    export_start, export_end = _compute_extraction_window(export_end_refernce_datetime=export_end_reference_datetime, days_delay=days_delay, days_export=days_export)
+    records = _extract_records_from_file_url(url=url, export_start=export_start, export_end=export_end, datetime_format=datetime_format, cache_dir=cache_dir)
+
+    metadata ={
+        "days_delay": days_delay,
+        "days_export": days_export,
+        "url": url,
+        "export_datetime_utc_start": export_start.strftime(datetime_format),
+        "export_datetime_utc_end": export_end.strftime(datetime_format),
+        "datetime_format": datetime_format,
+        "num_unique_samples_per_time_series": len(records["HourUTC"].unique()),
+    }
+
+    return records, metadata
+
 def _compute_extraction_window(export_end_reference_datetime: datetime.datetime, days_delay: int, days_export: int) -> Tuple[datetime.datetime, datetime.datetime]:
     """Compute the extraction window relative to 'export_end_reference_datetime' and take into consideration the maximum and minimum data points available in the dataset."""
 
