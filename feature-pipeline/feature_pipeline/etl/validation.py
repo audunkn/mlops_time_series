91c43509e2180fa9c8f7cc3e7ac35c63089dd9d1
audunkn <audun.klarholm.nilsen@gmail.com>
2025-01-01 15:55:12 +0100

    added 5 expectations to def build_expectation_sute

--- feature-pipeline/feature_pipeline/etl/validation.py
+++ feature-pipeline/feature_pipeline/etl/validation.py
@@ -23,3 +23,26 @@ def build_expectaion_suite() -> ExpectationSuite:
             },
         )
     )
+    expectation_suite_energy_consumption.add_expectation(
+        ExpectationConfiguration(
+            expectation_type="expect_table_column_count_to_equal", kwargs={"value": 4}
+        )
+    )
+
+    # Datetime UTC
+    expectation_suite_energy_consumption(
+        ExpectationConfiguration(
+            expectation_type="expect_columns_values_to_not_be_null",
+            kwargs{"column": "datetime_utc"},
+        )
+    )
+
+    # Area
+    expectation_suite_energy_consumption(
+        ExpectationConfiguration(
+            expectation_type="expect_column_distinct_values_to_be_in_set",
+            kwargs={"column": "area", "value_set": (0, 1, 2)},
+        )
+    )
+
+
