a50b655cc77c34ff9c024f9b4c63f680538a04e9
audunkn <audun.klarholm.nilsen@gmail.com>
2025-01-01 15:37:38 +0100

    created def build_expectation_suite

--- feature-pipeline/feature_pipeline/etl/validation.py
+++ feature-pipeline/feature_pipeline/etl/validation.py
@@ -1,2 +1,25 @@
 from great_expectations_core import ExpectationSuite, ExpectationConfiguration
 
+def build_expectaion_suite() -> ExpectationSuite:
+    """
+    Builder used to retrieve an instance of the validation expectation suite.
+    """
+
+    expectation_suite_energy_consumption = ExpectationSuite(
+        expectation_suite_name = "energy_consuption_suite"
+    )
+
+    # Columns
+    expectation_suite_energy_consumption.add_expectation(
+        ExpectationConfiguration(
+            expectation_type="expect_table_columns_to_match_ordered_list",
+            kwargs={
+                "column_list": [
+                    "datetime_utc",
+                    "area",
+                    "consumer_type",
+                    "energy_comsuption",
+                ]
+            },
+        )
+    )
